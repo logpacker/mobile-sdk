c2f0426f116b45099084dbb548e4111a3cdcb3af
Aliaksandr Pliutau <pliutau.aliaksandr@lazada.com>
2016-03-03 17:19:34 +0700

    #7. Send function

--- wp-logpackermobilesdk/LogPackerMobileSDK.cs
+++ wp-logpackermobilesdk/LogPackerMobileSDK.cs
@@ -17,8 +17,33 @@ namespace logpackermobilesdk
 		}
 	}
 
+	public struct Event
+	{
+		public string Message;
+		public string Source;
+		public int LogLevel;
+		public string UserID;
+		public string UserName;
+
+		public Event(string message, string source, int level, string userID, string userName) {
+			Message = message;
+			Source = source;
+			LogLevel = level;
+			UserID = userID;
+			UserName = userName;
+		}
+	}
+
 	public class LogPackerMobileSDK
 	{
+		// LogLevel constants
+		public const int FatalLogLevel = 0;
+		public const int ErrorLogLevel = 1;
+		public const int WarnLogLevel = 2;
+		public const int InfoLogLevel = 3;
+		public const int DebugLogLevel = 4;
+		public const int NoticeLogLevel = 5;
+
 		public LogPackerMobileSDK ()
 		{
 		}
@@ -51,6 +76,33 @@ namespace logpackermobilesdk
 			Client client = new Client (clusterURL, environment, agent);
 
 			return client;
+		}
+
+		public bool Send(Event e)
+		{
+			Validate (e);
+			string json = GenerateRequestJSONString (e);
+			return true;
+		}
+
+		private bool Validate(Event e)
+		{
+			if (e.Message == "") {
+				throw new ArgumentException("Message cannot be empty");
+			}
+			if (e.LogLevel < FatalLogLevel || e.LogLevel > NoticeLogLevel) {
+				throw new ArgumentException("LogLevel is invalid. Valid are: "+FatalLogLevel+" - "+NoticeLogLevel);
+			}
+
+			return true;
+		}
+
+		private string GenerateRequestJSONString(Event e)
+		{
+			return @"{
+				""client"":{},
+				""messages"":[]
+			}";
 		}
 	}
 }
